1f6526d2c4176e0b614901ebb774eaecf67e1b9d
grubnubble <montanalisagoodman@gmail.com>
2019-09-03 13:56:26 -0400

    Add route to print data from incoming notifications

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 import sqreen
 
 sqreen.start()
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def hello():
 	return 'hello, world!'
+
+@app.route('/notify', methods=["POST"])
+def notify():
+	if request.data:
+		# simple check to keep in mind that we 
+		# 	need to validate the request in some way
+		print request.data
+		return request.data
+	else:
+		return 'bad request!', 400

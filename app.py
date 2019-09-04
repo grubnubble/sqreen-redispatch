010beca76bc0bc33fc01f3b0df19d79c037256a1
grubnubble <montanalisagoodman@gmail.com>
2019-09-04 10:05:21 -0400

    Check signature for incoming notifications
    - Uses a helper function from Sqreen docs
    - was not able to pipenv install hmac or hashlib libraries

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, request
+import hmac
+import hashlib
 import sqreen
 
 sqreen.start()
 app = Flask(__name__)
+SECRET = '59e1b59394ca7450f3d4ae0db3fb4083'
+
+def check_signature(secret_key, request_signature, request_body):
+	"""Helper function from Sqreen docs"""
+
+	hasher = hmac.new(secret_key, request_body, hashlib.sha256)
+	dig = hasher.hexdigest()
+
+	return hmac.compare_digest(dig, request_signature)
 
 @app.route('/')
 def hello():
@@ -10,13 +21,16 @@ def hello():
 
 @app.route('/notify', methods=["POST"])
 def notify():
-	if request.data:
-		# simple check to keep in mind that we 
-		# 	need to validate the request in some way
+	request_signature = str(request.headers.get('X-Sqreen-Integrity'))
+	if check_signature(SECRET, request_signature, request.get_data()):
 		app.logger.info("Notification from Sqreen: %s", request.data)
+		print(request.data)
 		return request.data
 	else:
 		return 'bad request!', 400
+
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -8,10 +8,14 @@ def test_hello():
 
 def test_notify():
 	data = "here is some data"
-	response = client.post("notify", data=data)
+	hasher = hmac.new(SECRET, data, hashlib.sha256)
+	good_token = hasher.hexdigest()
+	response = client.post("notify", data=data, headers={'X-Sqreen-Integrity': good_token})
+	
 	assert response.status_code == 200
 	assert response.data == data
 
-def test_notify_fails_appropriately():
-	response = client.post("notify")
+def test_notify_fails_with_bad_signature():
+	bad_token = 'xxxxx'
+	response = client.post("notify", headers={'X-Sqreen-Integrity': bad_token})
 	assert response.status_code == 400
